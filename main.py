6ed2a825cffed20ab5f0c5c4e62ff4f6ba90b295
JeffPjj <110723174+JeffPjj@users.noreply.github.com>
2022-08-07 22:22:18 +0800

    Update main.py
    新增几个模块

--- main.py
+++ main.py
@@ -10,18 +10,25 @@ from datetime import timezone
 
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+her_birthday = os.environ['HER_BIRTHDAY']
+my_birthday = os.environ['MY_BIRTHDAY']
+
+
 big_mother_day = os.environ['BIG_MOTHER_DAY']
+big_mother_day_leave = os.environ['BIG_MOTHER_DAY_LEAVE']
+
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id1 = os.environ["USER_ID1"]
 user_id2 = os.environ["USER_ID2"]
 
-template_id = os.environ["TEMPLATE_ID"]
+weather_template_id = os.environ["WEATHER_TEMPLATE_ID"]
+date_template_id = os.environ["DATE_TEMPLATE_ID"]
+funny_template_id = os.environ["FUNNY_TEMPLATE_ID"]
+wwzc_template_id = os.environ["WWZC_TEMPLATE_ID"]
 
 tain_xing_api_key = os.environ["TX_API_KEY"]
-
 
 
 def get_tody():
@@ -36,6 +43,7 @@ def get_tody():
     # 北京时间
     beijing_now = utc_now.astimezone(SHA_TZ)
     return datetime.strptime(str(beijing_now).split(" ")[0], "%Y-%m-%d")
+
 
 def get_weekday(today_date):
     num = today_date.weekday()
@@ -62,11 +70,52 @@ def get_tai_ci():
         return "今天台词说不出来"
 
 
+def get_tian_gou():
+    url = "http://api.tianapi.com/tiangou/index?key=" + tain_xing_api_key
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()['newslist'][0]['content']
+    else:
+        return "今天不当添购了哼哼！"
+
+
+def get_cai_zi_mi():
+    url = "http://api.tianapi.com/zimi/index?key=" + tain_xing_api_key
+    response = requests.get(url)
+    if response.status_code == 200:
+        data_dict = response.json()['newslist'][0]
+        return data_dict['content'], data_dict['answer'], data_dict['reason']
+    else:
+        return "今天字谜路了"
+
+
+def get_wyy_comment():
+    url = "http://api.tianapi.com/hotreview/index?key=" + tain_xing_api_key
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()['newslist'][0]['content']
+    else:
+        return "今天网易云emo了..."
+
+
 def get_weather():
     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
     res = requests.get(url).json()
     weather = res['data']['list'][0]
-    return weather['weather'], math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high'])
+    return weather['weather'], math.floor(weather['wind']), math.floor(weather['temp']), math.floor(weather['low']), math.floor(weather['high']), math.floor(weather['airQuality']), math.floor(weather['humidity'])
+
+
+def get_weather_notice(weather):
+    if weather == "晴":
+        return "小刘~ 今天有太阳，注意防晒哦！"
+    elif weather == "阴":
+        return "小刘~ 阴天！在不开灯的房间..."
+    elif weather == "多云":
+        return "小刘~ 今天是阴晴不定的一天哦"
+    elif weather == "雨":
+        return "小刘~ 今天有雨，出门记得带伞！"
+    else:
+        return "小刘~ 俺今天就不叨叨了！"
 
 
 def get_jin_shan(date):
@@ -84,7 +133,7 @@ def get_love_days(today_date):
     return delta.days
 
 
-def get_birthday(today_date):
+def get_birthday(today_date,  birthday):
     next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
     if next < today_date:
         next = next.replace(year=next.year + 1)
@@ -101,38 +150,148 @@ def get_words():
 def get_random_color():
     return "#%06x" % random.randint(0, 0xFFFFFF)
 
+
 def get_next_mother_day(today_date):
     return 30 - (today_date - datetime.strptime(big_mother_day, "%Y-%m-%d")).days
 
-def run(user_id):
+
+"""
+根据上一次来的和走的时间，与现在时间对比，如果正在大姨妈，环境里的配置是big_mother_day=True，big_mother_day_leave=False
+如果非姨妈期间，big_mother_day和big_mother_day_leave是具体时间
+"""
+def get_big_mother_value(big_mother_day, big_mother_day_leave, today_date):
+    if big_mother_day == "True":
+        return "正在经期中...", special_text(), "", "正在经期中...", special_text()
+    else:
+        next_mother_day = get_next_mother_day(today_date)
+        if next_mother_day < 0:
+            return big_mother_day, special_text("-", "#000000"), big_mother_day_leave, "延迟 {days} ".format(days=str(next_mother_day)), special_text("天", "#000000")
+        else:
+            return big_mother_day, special_text("-", "#000000"), big_mother_day_leave, str(next_mother_day), special_text("天后", "#000000")
+
+
+"""
+用于特殊情况处理 ： 还有{{10}} {{天}}
+"""
+def special_text(text="", color="#000000"):
+    if text == "":
+        return {"value": text}
+    else:
+        return {"value": text, "color": color}
+
+"""
+next_mother_day是获取环境里的上一次经期时间，用当前日期减去上一次的日期得出的天数
+"""
+def get_mother_day_notice(big_mother_day, today_date):
+    if big_mother_day == "True":
+        return "姨妈ing，别想冷饮了，吨吨吨热水吧！"
+    else:
+        next_mother_day = get_next_mother_day(today_date)
+        if 1 < next_mother_day <= 7:
+            return "大姨妈快来啦，不能再吨吨吨地喝冷饮咯！"
+        elif 7 < next_mother_day < 12:
+            return "大姨妈刚走哦，别一下子吨吨吨喝太多冷饮哦！"
+        elif next_mother_day <= 0:
+            return "姨妈延期了{days}天诶，别担心，保持愉悦心情和正常作息即可！".format(days=next_mother_day)
+        else:
+            return "大姨妈旅游去了，该吃吃该喝喝！"
+
+
+def weather_sender(wm, user_id, template_id):
+    # 获取时间
     today_date = get_tody()
     today_str = str(today_date).split(" ")[0]
     weekday = get_weekday(today_date)
-    love_days = get_love_days(today_date)
-    weather, temperature, min_temperature, max_temperature = get_weather()
-    next_big_mother_day = get_next_mother_day(today_date)
-    cai_hong_pi = get_cai_hong_pi()
-    jin_shan_en, jin_shan_zh = get_jin_shan(today_str)
 
-    client = WeChatClient(app_id, app_secret)
-    wm = WeChatMessage(client)
+    # 获取天气相关
+    weather, wind, temperature, min_temperature, max_temperature, air_quality, humidity = get_weather()
+
+    # 获取天气提醒
+    weather_notice = get_weather_notice(weather)
+
     data = {
         "today": {"value": today_str, "color": "#f4cccc"},
         "weekday": {"value": weekday, "color": "#76a5af"},
-        "love_days": {"value": love_days, "color": "#ea9999"},
+        "city": {"value": city, "color": "#436EEE"},
         "weather": {"value": weather, "color": "#e69138"},
         "temperature": {"value": temperature, "color": "#674ea7"},
         "min_temperature": {"value": min_temperature, "color": "#3d85c6"},
+        "wind": {"value": wind, "color": "#3CB371"},
         "max_temperature": {"value": max_temperature, "color": "#a64d79"},
-        "big_mother_day": {"value": big_mother_day, "color": "#a61c00"},
-        "next_big_mother_day": {"value": next_big_mother_day, "color": "#6aa84f"},
-        "cai_hong_pi": {"value": cai_hong_pi, "color": "#a4c2f4"},
-        "jin_shan_en": {"value": jin_shan_en, "color": "#6fa8dc"},
-        "jin_shan_zh": {"value": jin_shan_zh, "color": "#c9daf8"}
+        "air_quality": {"value": air_quality, "color": "#CD6839"},
+        "humidity": {"value": humidity, "color": "#6495ED"},
+        "weather_notice": {"value": weather_notice, "color": "#6495ED"},
     }
     print(data)
     res = wm.send_template(user_id, template_id, data)
     print(res)
 
+
+def date_menage_sender(wm, user_id, template_id):
+    today_date = get_tody()
+
+    love_days = get_love_days(today_date)
+    until_her_birthday = get_birthday(today_date, her_birthday)
+    until_my_birthday = get_birthday(today_date, my_birthday)
+
+    # TODO:next_big_mother_day 根据天数变换颜色
+    b_m_d, text1, b_m_d_l, next_big_mother_day, text2 = get_big_mother_value(big_mother_day, big_mother_day_leave, today_date)
+
+    data = {
+        "love_days": {"value": love_days, "color": "#ea9999"},
+        "until_her_birthday": {"value": until_her_birthday, "color": "#E9967A"},
+        "until_my_birthday": {"value": until_my_birthday, "color": "#166985"},
+        "big_mother_day": {"value": b_m_d, "color": "#A52A2A"},
+        "text1": text1,
+        "big_mother_day_leave": {"value": big_mother_day_leave, "color": "#E69E9E"},
+        "next_big_mother_day": {"value": next_big_mother_day, "color": "#6aa84f"},
+        "text2": text2
+    }
+    print(data)
+    res = wm.send_template(user_id, template_id, data)
+    print(res)
+
+def funny_sender(wm, user_id, template_id):
+    cai_hong_pi = get_cai_hong_pi()
+    tian_gou = get_tian_gou()
+    zi_mi = get_cai_zi_mi()
+
+    data = {
+        "cai_hong_pi": {"value": cai_hong_pi, "color": "#FF6666"},
+        "tian_gou": {"value": tian_gou, "color": "#A64DFF"},
+        "zi_mi": {"value": zi_mi, "color": "#990000"},
+    }
+    print(data)
+    res = wm.send_template(user_id, template_id, data)
+    print(res)
+
+
+def wwzc_sender(wm, user_id, template_id):
+    # 获取时间
+    today_date = get_tody()
+    today_str = str(today_date).split(" ")[0]
+
+    lines = get_tai_ci()
+    jin_shan_en, jin_shan_zh = get_jin_shan(today_str)
+    wyy_comment = get_wyy_comment()
+
+    data = {
+        "lines": {"value": lines, "color": "#004D99"},
+        "jin_shan_en": {"value": jin_shan_en, "color": "#6fa8dc"},
+        "jin_shan_zh": {"value": jin_shan_zh, "color": "#c9daf8"},
+        "wyy_comment": {"value": wyy_comment, "color": "#D8648B"}
+    }
+    print(data)
+    res = wm.send_template(user_id, template_id, data)
+    print(res)
+
+def run(user_id):
+    client = WeChatClient(app_id, app_secret)
+    wm = WeChatMessage(client)
+    weather_sender(wm, user_id, weather_template_id)
+    date_menage_sender(wm, user_id, date_template_id)
+    funny_sender(wm, user_id, funny_template_id)
+    wwzc_sender(wm, user_id, wwzc_template_id)
+
 run(user_id1)
-run(user_id2)
+# run(user_id2)
